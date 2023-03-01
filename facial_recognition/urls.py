07938f4ea7cd098413b4c3d527e5c2df97bdaccd
Antony20 <antomutwiri2001@gmail.com>
2023-03-01 22:45:28 +0300

    adding some urls

--- facial_recognition/urls.py
+++ facial_recognition/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', recog_views.home, name='home'),
+    path('dashboard/', recog_views.dashboard, name='dashboard'),
 ]
